6561cf6025cbe517cccea2892d7bbd9f6c261b68
kehuang <kehuang@box.com>
2015-02-02 22:17:46 -0800

    Initial Commit

---
+++ rottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes
+//
+//  Created by Ke Huang on 2/2/15.
+//  Copyright (c) 2015 Ke Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
